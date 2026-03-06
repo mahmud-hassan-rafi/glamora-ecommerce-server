5df47790dccd774e6b401501564237005848e691
Mahmud Hassan Rafi <mahmudhassanrafi12@gmail.com>
2026-03-06 11:47:37 +0600

    feat: created express server

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+import app from "./app";
+import http from "http";
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`server running at PORT ${PORT}`));
